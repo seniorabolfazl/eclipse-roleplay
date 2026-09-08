b67007a1f782f09549c4a6336c18163fbe7d3cb7
seniorabolfazl <abolfazlmirzaie86@gmail.com>
2026-09-08 15:32:08 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// If you deploy to https://<username>.github.io/<repo-name>/, base MUST
-// match "/<repo-name>/" exactly (with slashes), or assets will 404.
-// If you deploy to a user/org root site (<username>.github.io) or a
-// custom domain, set base back to '/'.
 export default defineConfig({
   plugins: [react()],
   base: '/eclipse-roleplay/',
